26e31f03e84963607a71164d006fd00eaebc4b61
Trusova Ekaterina <trusovae325@gmail.com>
2025-03-05 14:53:08 +0300

    Update 16_Jackets.py

--- 16_Jackets.py
+++ 16_Jackets.py
@@ -1,35 +1,13 @@
 import numpy as np
 from scipy.optimize import linprog
 
-c = [-2500, -2000] 
-
-A = [
-    [5, 3],  
-]
-b = [525]  
-
-x0_bounds = (0, 100)  
-x1_bounds = (0, 75)  
-
-result = linprog(c, A_ub=A, b_ub=b, bounds=[x0_bounds, x1_bounds], method='highs')
-
-if result.success:
-    optimal_catch = result.x
-    optimal_profit = -result.fun 
-    print(f"Оптимальное количество курток: {optimal_catch[0]:.2f}")
-    print(f"Оптимальное количество пиджаков: {optimal_catch[1]:.2f}")
-    print(f"Максимальная прибыль: {optimal_profit:.2f} рублей")
-else:
-    print("Не удалось найти оптимальное решение.")
-
-
 def greedy_jacket_production():
-    total_hours = 525 
-    hours_per_jacket = 5  
-    hours_per_blazer = 3 
-    max_jackets_demand = 100 
-    max_blazers_supply = 75 
-    profit_per_jacket = 2500 
+    total_hours = 525
+    hours_per_jacket = 5
+    hours_per_blazer = 3
+    max_jackets_demand = 100
+    max_blazers_supply = 75
+    profit_per_jacket = 2500
     profit_per_blazer = 2000
 
     profit_per_hour_jacket = profit_per_jacket / hours_per_jacket
@@ -59,5 +37,26 @@ def greedy_jacket_production():
     print(f"Оптимальное количество пиджаков: {blazers_produced}")
     print(f"Максимальная прибыль: {total_profit} рублей")
 
-greedy_jacket_production()
+if __name__ == "__main__":
+    c = [-2500, -2000]
 
+    A = [
+        [5, 3],
+    ]
+    b = [525]
+
+    x0_bounds = (0, 100)
+    x1_bounds = (0, 75)
+
+    result = linprog(c, A_ub=A, b_ub=b, bounds=[x0_bounds, x1_bounds], method='highs')
+
+    if result.success:
+        optimal_catch = result.x
+        optimal_profit = -result.fun
+        print(f"Оптимальное количество курток: {optimal_catch[0]:.2f}")
+        print(f"Оптимальное количество пиджаков: {optimal_catch[1]:.2f}")
+        print(f"Максимальная прибыль: {optimal_profit:.2f} рублей")
+    else:
+        print("Не удалось найти оптимальное решение.")
+
+    greedy_jacket_production()
