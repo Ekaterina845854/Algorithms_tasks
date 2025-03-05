3e906e90dbf398f37bc6e92f1ca8301e5118f7d8
Trusova Ekaterina <trusovae325@gmail.com>
2025-03-05 14:44:34 +0300

    Update 3_Table_View.py

--- 3_Table_View.py
+++ 3_Table_View.py
@@ -5,11 +5,11 @@ def quicksort(arr, values):
     left = [(arr[i], values[i]) for i in range(len(arr)) if arr[i] < pivot]
     middle = [(arr[i], values[i]) for i in range(len(arr)) if arr[i] == pivot]
     right = [(arr[i], values[i]) for i in range(len(arr)) if arr[i] > pivot]
-    
+
     left_sorted, left_values = quicksort([x[0] for x in left], [x[1] for x in left])
     middle_sorted, middle_values = [x[0] for x in middle], [x[1] for x in middle]
     right_sorted, right_values = quicksort([x[0] for x in right], [x[1] for x in right])
-    
+
     return left_sorted + middle_sorted + right_sorted, left_values + middle_values + right_values
 
 def downsample(times, values, N):
@@ -18,7 +18,7 @@ def downsample(times, values, N):
         raise ValueError("Длины times и values должны совпадать")
     if N < 2:
         raise ValueError("N >= 2")
-    
+
     times, values = quicksort(times, values)
 
     step = (len(times) - 1) / (N - 1)
@@ -27,9 +27,9 @@ def downsample(times, values, N):
 
     new_values = [values[0]]
     for i in range(1, N - 1):
-        start, end = indices[i-1], indices[i + 1] 
-        avg_value = sum(values[start : end + 1]) / (end - start + 1)  
+        start, end = indices[i-1], indices[i + 1]
+        avg_value = sum(values[start : end + 1]) / (end - start + 1)
         new_values.append(avg_value)
-    new_values.append(values[-1]) 
+    new_values.append(values[-1])
 
     return new_times, new_values
