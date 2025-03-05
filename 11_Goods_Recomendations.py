728a77d47d30b189d147c4a30d6e1f8b8cabe5e8
Trusova Ekaterina <trusovae325@gmail.com>
2025-03-05 14:50:49 +0300

    Update 11_Goods_Recomendations.py

--- 11_Goods_Recomendations.py
+++ 11_Goods_Recomendations.py
@@ -1,45 +1,46 @@
 """
 Вопросы
-- Критерий похожести? Похожими считаются товары в одной связной компоненте? Первые соседи? До k-го элемента? По порядку сортировки
+- Критерий похожести? Похожими считаются первые соседи? До k-го элемента?
 - Критерий сортировки? В реализации за основу взята близость к таргету
 """
 
 from collections import deque
 
 def bfs(graph, start):
-    visited = set()  
-    queue = deque([start])  
-    recommendations = []  
+    visited = set()
+    queue = deque([start])
+    recommendations = []
 
     while queue:
-        current = queue.popleft()  
+        current = queue.popleft()
         if current not in visited:
-            visited.add(current) 
-            for neighbor in graph.get(current, []):  
+            visited.add(current)
+            for neighbor in graph.get(current, []):
                 if neighbor not in visited:
-                    recommendations.append(neighbor) 
-                    queue.append(neighbor) 
+                    recommendations.append(neighbor)
+                    queue.append(neighbor)
     return recommendations
 
+if __name__ == "__main__":
 
-goods = {                                           
-    "Дрель": ["Молоток"],                             
-    "Молоток": ["Дрель", "Пила", "Гвозди"],           
-    "Пила": ["Молоток", "Нож"],                       
-    "Нож": ["Пила", "Вилка", "Ложка"],                
-    "Вилка": ["Нож"],                                 
-    "Ложка": ["Нож"],                                 
-    "Гвозди": ["Молоток", "Булава", "Иглы",           
-               "Серьги-гвоздики"],                    
-    "Булава": ["Гвозди", "Иглы"],                     
-    "Иглы": ["Булава", "Гвозди"],                     
-    "Серьги-гвоздики": ["Кольца", "Заколка",          
-                        "Гвозди"],                    
-    "Кольца": ["Серьги-гвоздики"],                    
-    "Заколка": ["Серьги-гвоздики", "Расческа"],       
-    "Расческа": ["Заколка"],                          
-}      
+    goods = {
+        "Дрель": ["Молоток"],
+        "Молоток": ["Дрель", "Пила", "Гвозди"],
+        "Пила": ["Молоток", "Нож"],
+        "Нож": ["Пила", "Вилка", "Ложка"],
+        "Вилка": ["Нож"],
+        "Ложка": ["Нож"],
+        "Гвозди": ["Молоток", "Булава", "Иглы",
+                "Серьги-гвоздики"],
+        "Булава": ["Гвозди", "Иглы"],
+        "Иглы": ["Булава", "Гвозди"],
+        "Серьги-гвоздики": ["Кольца", "Заколка",
+                            "Гвозди"],
+        "Кольца": ["Серьги-гвоздики"],
+        "Заколка": ["Серьги-гвоздики", "Расческа"],
+        "Расческа": ["Заколка"],
+    }
 
 
-recommendations = bfs(goods, "Нож")
-print(recommendations)
+    recommendations = bfs(goods, "Нож")
+    print(recommendations)
